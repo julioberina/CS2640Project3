207f0dd986f338a2bb63993d522821a2d081f4a6
Julio Berina <julioberina@gmail.com>
2018-11-09 01:08:26 -0800

    Space out code and add comments to subroutines

--- binary_search.asm
+++ binary_search.asm
@@ -43,7 +43,7 @@ main:	# program entry
      li $t4, 0
      li $t5, 0
 
-getinputs:
+getinputs:     # Grab an integer input
      la $s2, array
 
      li $v0, 5
@@ -52,10 +52,15 @@ getinputs:
      move $t4, $v0     # t4 stores integer input
 
 check:    # check if current < array element
+     # If index after last item is reached, load current to that index
      beq $t2, $t1, arrayload
+
      lw $t3, 0($s2)
      slt $t0, $t4, $t3
+
      bne $t0, $zero, swap
+
+     # Increment counters and iterators
      addi $t2, $t2, 1
      addi $s2, $s2, 4
      j check
@@ -69,14 +74,16 @@ swap:     # swap current value with array element
      addi $s2, $s2, 4
      j check
 
-arrayload:
+arrayload:     # Load element into the address after the last element
      sw $t4, 0($s2)
      addi $s2, $s2, 4
      addi $t1, $t1, 1
      li $t2, 0
      bne $t1, $s0, getinputs
 
-readyarray:
+# $s2 is pointing to the address after the last item
+
+readyarray:     # Set $s4 to address of the last item
      addi $s4, $s2, -4
      li $t5, 0
      la $a0, terpri
@@ -84,7 +91,9 @@ readyarray:
      syscall
      la $s2, array
 
-printarray:
+# Go back to the start and get ready to print all array items
+
+printarray:     # Print all array items
      lw $a0, 0($s2)
      li $v0, 1
      syscall
@@ -95,7 +104,7 @@ printarray:
      addi $t5, $t5, 1
      bne $t5, $s0, printarray
 
-finditem:
+finditem:     # Gather input for item to look for
      la $a0, intsearch
      li $v0, 4
      syscall
@@ -109,13 +118,13 @@ finditem:
      addiu $t2, $s4, 0    # t2 set to last - 1
      li $t5, 1            # Defaults to 1, which means value found, 0 not found
 
-     jal binarysearch
+     jal binarysearch     # Do a binary search
 
-     move $a0, $t5
+     move $a0, $t5        # Load t5 in a0, output 1 or 0 whether or not value was found
      li $v0, 1
      syscall
 
-     j finditem
+     j finditem           # Repeat
 
 binarysearch:
      li $t7, 4
@@ -134,15 +143,17 @@ binarysearch:
 search:
      sra $t0, $t0, 3
      sll $t0, $t0, 2
-     addu $t4, $t1, $t0
-     lw $t0, 0($t4)
-     beq $s3, $t0, return
+     addu $t4, $t1, $t0         # Calculate midpoint, offset from left index
+     lw $t0, 0($t4)             # Store value of midpoint of array
+
+     beq $s3, $t0, return       # if array[mid] == value, return back to caller
+
      slt $t6, $s3, $t0
      li $t7, 1
      beq $t6, $t7, look_left    # Keep searching to the left
 
 look_right:
-     addiu $t1, $t4, 4
+     addiu $t1, $t4, 4    # Right bsearch, left = mid+1
      jal binarysearch     # Recursive call to binary search
      j return             # Finished, return back to caller
 
